08d330dc52ae3597666894aa0672b7cbc1beef70
Lucas <oliveira.mesquita@aluno.uece.br>
2019-04-28 02:26:31 -0300

    Iniciada criação de migrates (DDL)

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const db = require('./src/config/db')
 
 const app = express()
 const port = process.env.PORT || 3001;
 const domain = 'localhost'
+
+// app.db = db
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello')
